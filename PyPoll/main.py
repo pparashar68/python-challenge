52ab919e711be98285e8a698fd9b28a05f3c463f
Pankaj Parashar <pparashar68@gmail.com>
2020-09-23 21:42:24 -0700

    Corrected the logic to count Total Votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,18 +12,15 @@ candidate1_counter = 0
 candidate2_counter = 0
 candidate3_counter = 0
 candidate4_counter = 0
-cnt = 1
+cnt = 0
 
 with open(election_input_file,'r') as election_input_file_filehandler:
     election_voting_file = csv.reader(election_input_file_filehandler,delimiter=',')
     next
-    print(election_voting_file)
     for voter_record in election_voting_file:
-        #print(voter_record)
         voter_id = voter_record[0]
         voter_country = voter_record[1]
         voter_candidate = voter_record[2]
-        total_votes = total_votes + int(cnt)
         cnt = cnt + 1
         if voter_candidate == candidate1:
             candidate1_counter = candidate1_counter + 1
@@ -33,8 +30,13 @@ with open(election_input_file,'r') as election_input_file_filehandler:
             candidate3_counter = candidate3_counter + 1
         elif voter_candidate == candidate4:
             candidate4_counter = candidate4_counter + 1
+        total_votes = candidate1_counter + candidate2_counter + candidate3_counter + candidate4_counter
 
 
+print("Election Results ")
+print('=' * 40)
+print(f'Total Votes: {total_votes}')
+print('=' * 40)
 print(f'{candidate1} votes :{candidate1_counter}')
 print(f'{candidate2} votes :{candidate2_counter}')
 print(f'{candidate3} votes :{candidate3_counter}')
