f6bde402f353c5cd990ac47e21ddbbe1682b552a
Pankaj Parashar <pparashar68@gmail.com>
2020-09-23 22:05:53 -0700

    Added percentage count for each candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -32,6 +32,24 @@ with open(election_input_file,'r') as election_input_file_filehandler:
             candidate4_counter = candidate4_counter + 1
         total_votes = candidate1_counter + candidate2_counter + candidate3_counter + candidate4_counter
 
+if total_votes > 0:
+    candidate1_percentage_votes = candidate1_counter / total_votes
+    candidate2_percentage_votes = candidate2_counter / total_votes
+    candidate3_percentage_votes = candidate3_counter / total_votes
+    candidate4_percentage_votes = candidate4_counter / total_votes
+else:
+    print(" No Voting Happen") 
+
+if candidate1_counter > candidate2_counter and candidate1_counter > candidate3_counter and candidate1_counter > candidate4_counter:
+    winner= "Khan"
+elif candidate2_counter > candidate1_counter and candidate2_counter > candidate3_counter and candidate1_counter > candidate4_counter:
+    winner = "Correy"     
+elif candidate3_counter > candidate1_counter and candidate3_counter > candidate2_counter and candidate3_counter > candidate4_counter:
+    winner = "Li"
+elif candidate4_counter > candidate1_counter and candidate4_counter > candidate2_counter and candidate4_counter > candidate3_counter:
+    winner = "O'Tooley"
+
+     
 
 print("Election Results ")
 print('=' * 40)
@@ -41,6 +59,11 @@ print(f'{candidate1} votes :{candidate1_counter}')
 print(f'{candidate2} votes :{candidate2_counter}')
 print(f'{candidate3} votes :{candidate3_counter}')
 print(f'{candidate4} votes :{candidate4_counter}')
+print('=' * 40)
+print(f'Winner: {winner}')
+print('=' * 40)
+
+
 
         
 
